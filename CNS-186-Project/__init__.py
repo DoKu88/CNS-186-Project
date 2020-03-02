0a6af0359f4984142fe1c6915452b59db8158159
Marcus <mddoming@caltech.edu>
2020-03-02 07:41:30 -0800

    making pip package

---
+++ CNS-186-Project/__init__.py
@@ -0,0 +1,5 @@
+from .cub_bird_class import *
+from .data_aug import *
+from .data_split import *
+from .file_mover import *
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='CNS-186-Project',
+    version='0.0.1', 
+    description='active learning vs random batch learning for image classification', 
+    url='git@github.com:DoKu88/CNS-186-Project.git', 
+    author='Marcus Dominguez-Kuhne aka Marcus Doku', 
+    author_email='Follow me on Github first yee', 
+    license='unlicense', 
+    packages=['CNS-186-Project'], 
+    zip_safe=False
+)
+
